ca8c1d188a8637423f3b2ab33fca83f32a1d584b
unknown <Alexander.hirtl@it.htl-hl.ac.at>
2026-02-24 16:41:49 +0100

    fixed create.sql

--- create.sql
+++ create.sql
@@ -1,7 +1,7 @@
-drop database Studnenplan if exists;
+drop database if exists Stundenplan ;
 Create database Stundenplan;
 use Stundenplan;
-create table  User (
-    U_ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    Password varchar(255);
+create table User(
+	U_ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	Password varchar(255)
 );
