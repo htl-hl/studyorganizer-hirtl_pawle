c237af9bb5923ae16a564b2babba92becac759e4
unknown <Alexander.hirtl@it.htl-hl.ac.at>
2026-03-07 16:22:34 +0100

    added method for border colors depending on duetime

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -6,6 +6,25 @@
 
 use yii\helpers\Html;
 $this->title = 'Homepage';
+
+function getTaskDueDateClass(DateTime $dueDate): string
+{
+    $now  = new DateTime();
+    $diff = $now->diff($dueDate);
+
+    if ($dueDate < $now) {
+        return 'border-danger'; // überfällig
+    }
+
+    $daysLeft = (int) $diff->days;
+
+    return match(true) {
+        $daysLeft < 1  => 'border-danger',
+        $daysLeft < 7  => 'border-warning',
+        $daysLeft < 14 => 'border-primary',
+        default        => '',
+    };
+}
 ?>
 <div class="site-index">
 
@@ -18,12 +37,16 @@ $this->title = 'Homepage';
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
                     </svg>
                 </div>
-
             </div>
         </div>
+
         <?php foreach ($aufgaben as $aufgabe): ?>
+            <?php
+            $dueDate  = new DateTime($aufgabe->Faelligkeitsdatum);
+            $cssClass = getTaskDueDateClass($dueDate);
+            ?>
             <div class="col-4 p-3">
-                <div class="card" style="height: 100px;">
+                <div class="card <?= $cssClass ?> border-3  " style="height: 100px;">
                     <div class="card-body">
                         <h5 class="card-title">
                             <?= Html::a(
